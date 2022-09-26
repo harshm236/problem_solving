0a808d6b9c8c35c05ebb011c3234290ab133538b
harshm236 <109613150+harshm236@users.noreply.github.com>
2022-09-26 21:35:58 +0530

    card
    card

---
+++ card.js
@@ -0,0 +1,15 @@
+change = () =>
+{
+    let x = document.getElementById('card')
+
+    if(x.style.flexDirection='row')
+    {x.style.flexDirection='column'}
+    
+}
+change2 = () =>
+{
+    let x = document.getElementById('card')
+
+     if (x.style.flexDirection='column')
+    {x.style.flexDirection='row'}
+}
